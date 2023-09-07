27865558148d929bd0afb47dc9219d6dd3ed0776
Etheiry <lateluffy@gmail.com>
2023-09-07 03:34:59 -0400

    created taskmgr command

--- main.bat
+++ main.bat
@@ -4,6 +4,7 @@ echo Commands:
 echo		takeowner 			Take ownership of folder
 echo		kill 				kill any application
 echo		numgen				Number Generator
+echo		taskmgr				Takes out task manager
 echo.
 echo.
 
